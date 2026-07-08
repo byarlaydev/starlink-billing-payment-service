31543a1361142f8970e6bd51ab90867479024790
Byar Lay <byarlay@Byars-MacBook-Air-5.local>
2026-07-08 10:51:59 +0630

    Fix MessengerModule dependencies - import CustomersModule and BillingModule

--- backend/src/modules/messenger/messenger.module.ts
+++ backend/src/modules/messenger/messenger.module.ts
@@ -2,8 +2,11 @@ import { Module } from '@nestjs/common';
 import { MessengerService } from './messenger.service';
 import { MessengerController } from './messenger.controller';
 import { MessengerConversationService } from './messenger-conversation.service';
+import { CustomersModule } from '../customers/customers.module';
+import { BillingModule } from '../billing/billing.module';
 
 @Module({
+  imports: [CustomersModule, BillingModule],
   providers: [MessengerService, MessengerConversationService],
   controllers: [MessengerController],
   exports: [MessengerService, MessengerConversationService],
